7a06bee661c21016b0c2c22afe478ba5952ce23e
yedu <mailyeduraghav@gmail.com>
2024-07-04 14:42:10 -0400

    cleaned up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
-
 }
