ec059300b4810321e08c7f35b907e612ddcaeb6c
yedu <mailyeduraghav@gmail.com>
2024-07-05 13:15:52 -0400

    removed redundant dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     }
 }
 
